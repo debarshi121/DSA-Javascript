a0a0c7a7a7ea5c15fc6d5443ae4675e31b6bff4f
Debarshi Das <debarshi121@gmail.com>
2023-07-17 23:40:46 +0530

    Tower of Hanoi, Print All Possible Combinations

---
+++ Brute Force Solutions/LongestConsecutiveSubsequenceBF.js
@@ -0,0 +1,21 @@
+const findSub = (arr) => {
+    if(!arr.length) return 0;
+
+    const set = new Set(arr);
+    let maxLength = 1;
+    for(let i=0; i<arr.length; i++){
+        let inc = 1;
+        while(set.has(arr[i]+inc)) {
+            inc++;
+        }
+        if(inc>maxLength){
+            maxLength = inc;
+        }
+    }
+
+    return maxLength;
+}
+
+
+console.log(findSub([0,3,7,2,5,8,4,6,0,1]));
+console.log(findSub([]));
