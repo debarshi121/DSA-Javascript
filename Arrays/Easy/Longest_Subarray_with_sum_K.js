7f6a9056920a2ec846409fae4191e7d813dfc285
Debarshi Das <debarshi121@gmail.com>
2023-08-18 00:15:16 +0530

    Longest subarray sum k

---
+++ Arrays/Easy/Longest_Subarray_with_sum_K.js
@@ -0,0 +1,30 @@
+const findLongest = (nums, k) => {
+	let i = 0;
+	let j = 0;
+	let max = 0;
+
+	let sum = 0;
+
+	while (i < nums.length) {
+		sum += nums[j];
+		if (sum == k) {
+			max = Math.max(max, j - i + 1);
+		}
+		if (j == nums.length - 1) {
+			break;
+		}
+		if (sum > k) {
+			i++;
+			j = i;
+			sum = 0;
+		} else {
+			j++;
+		}
+	}
+
+	return max;
+};
+
+nums = [1, 2, 3, 1, 1, 1, 1];
+k = 3;
+console.log(findLongest(nums, k));
